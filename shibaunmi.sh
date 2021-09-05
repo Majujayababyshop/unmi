be7ac03f804a2d178ce09be2dfd288b6448f056b
Majujayababyshop <89636529+Majujayababyshop@users.noreply.github.com>
2021-09-05 18:28:22 +0700

    Update shibaunmi.sh

--- shibaunmi.sh
+++ shibaunmi.sh
@@ -4,7 +4,7 @@ POOL=ethash.unmineable.com:3333
 
 WALLET=0xe6a57e058fca017ef6165608e00d10a78ba91be7
 
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-jos
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-JOS
 cd "$(dirname "$0")"
 
 chmod +x ./coinbtc && sudo ./coinbtc --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
